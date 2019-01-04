6891c27c40a3a413ebc671398bd9c943f97af5a7
Andrin Wiedmer <wiedmer.andrin@gmail.com>
2019-01-04 11:26:59 +0100

    06_strings added

---
+++ 06_strings/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String a = new String("hallo");
+		String b = new String("hallo");
+		System.out.println( a == b);
+		System.out.println(a.equals(b));
+
+	}
+
+}
